503eab9ca430526dd9aec921196f061100362861
shefasf <bobko_2000@abv.bg>
2018-10-19 18:23:17 +0300

    Making boss patterns.
    Signed-off-by: shefasf <bobko_2000@abv.bg>

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float TimeToDestroy = 6;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if (TimeToDestroy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        TimeToDestroy -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [Header("Projectile Settings")]
+    public int NumberOfProjectiles;
+    public float ProjectileSpeed;
+    public GameObject ProjectilePrefab;
+    public float SecondForShooting = 1;
+    
+    public float AngleRotation = 0;
+
+    [Header("Private Variables")]
+    private Vector3 StartPoint;
+    private const float Radius = 1f;
+
+    private float TimeBeforeBullet = 0;
+
+    private float test = 0;
+    
+    private void Update()
+    {
+        if (this.TimeBeforeBullet > SecondForShooting)
+        {
+            StartPoint = transform.GetChild(0).position;
+            this.SpawnProjectile(this.NumberOfProjectiles);
+
+            this.TimeBeforeBullet = 0;
+        }
+
+        TimeBeforeBullet += Time.deltaTime;
+    }
+
+    private void SpawnProjectile(int number)
+    {
+        float angleStep = 360 / NumberOfProjectiles;
+        float angle = 0;
+
+        for (int i = 0; i <= NumberOfProjectiles - 1; i++)
+        {
+            float projectileDirXPosition = StartPoint.x + Mathf.Sin((angle * Mathf.PI + test) / 180) * Radius;
+            float projectileDirYPosition = StartPoint.y + Mathf.Cos((angle * Mathf.PI + test) / 180) * Radius;
+
+            Vector3 projectileVector = new Vector3(projectileDirXPosition, projectileDirYPosition, 0f);
+            Vector3 projectileMoveDirection = (projectileVector - StartPoint).normalized * ProjectileSpeed;
+
+            GameObject tmpObj = Instantiate(ProjectilePrefab, StartPoint, Quaternion.identity);
+            tmpObj.GetComponent<Rigidbody2D>().velocity = new Vector3(projectileMoveDirection.x, projectileMoveDirection.y, 0);
+
+            angle += angleStep;
+        }
+        test += this.AngleRotation;
+    }
+}
