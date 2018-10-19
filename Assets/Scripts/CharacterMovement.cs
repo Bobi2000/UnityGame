a82b6fcaf2b36454c6ec921f26bfcd8af9ac194a
shefasf <bobko_2000@abv.bg>
2018-10-19 15:17:22 +0300

    Added character Movement.
    Signed-off-by: shefasf <bobko_2000@abv.bg>

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public int MovementSpeed = 10;
+
+    private Rigidbody2D RigidBody;
+
+    public void Awake()
+    {
+        this.RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        float moveVectorX = Input.GetAxis("Horizontal");
+        float moveVectorY = Input.GetAxis("Vertical");
+        
+        this.transform.Translate(moveVectorX * this.MovementSpeed * Time.deltaTime, moveVectorY * this.MovementSpeed * Time.deltaTime, 0f);
+    }
+}
