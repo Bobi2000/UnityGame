36f15a6cc23b0823e60d14ed59fd891936173c5c
tast3blood <tast3bloodbg@gmail.com>
2018-10-21 17:24:07 +0300

    First Commit.
    Signed-off-by: tast3blood <tast3bloodbg@gmail.com>

---
+++ Assets/GrassMovment.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMovment : MonoBehaviour {
+
+    public BoxCollider2D Grass;
+
+    Animator anim;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim = GetComponent<Animator>();
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(Grass)
+        {
+
+            anim.SetInteger("Dist", 1);
+
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (Grass)
+        {
+
+            anim.SetInteger("Dist", 0);
+
+        }
+
+    }
+}
