d80c258b735483c1ecd2166b166df1684991f1da
shefasf <bobko_2000@abv.bg>
2018-10-27 18:27:07 +0300

    Fixing shooting.
    Signed-off-by: shefasf <bobko_2000@abv.bg>

---
+++ Assets/Scripts/CharacterBullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterBullet : MonoBehaviour
+{
+    public float TimeToDestroy = 6;
+
+    public ParticleSystem OnBulletDestroy;
+
+    public void Update()
+    {
+        if (TimeToDestroy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        TimeToDestroy -= Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!(collision.gameObject.name == "Character"))
+        {
+            Debug.Log("Bullet");
+
+            Instantiate(OnBulletDestroy, this.transform.position, Quaternion.identity);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterShooting.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class CharacterShooting : MonoBehaviour
+{
+    public float BulletSpeed = 200f;
+    public float AttackSpeed = 0.7f;
+
+    public Rigidbody2D projectile;
+
+    public Transform UpLauncher;
+    public Transform DownLauncher;
+    public Transform LeftLauncher;
+    public Transform RightLauncher;
+
+    private Vector3 direction;
+    private bool IsAttackReady = true;
+    private float CurrentAttack;
+
+    public void Start()
+    {
+        this.CurrentAttack = this.AttackSpeed;
+    }
+
+    public void FixedUpdate()
+    {
+        if (IsAttackReady)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                direction = UpLauncher.up;
+                this.Shoot(direction, UpLauncher);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                direction = LeftLauncher.right;
+                this.Shoot(direction, LeftLauncher);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                direction = RightLauncher.right * -1;
+                this.Shoot(direction, RightLauncher);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                direction = DownLauncher.up * -1;
+                this.Shoot(direction, DownLauncher);
+            }
+        }
+        else
+        {
+            if (this.CurrentAttack <= 0)
+            {
+                this.IsAttackReady = true;
+                this.CurrentAttack = this.AttackSpeed;
+            }
+            else
+            {
+                this.CurrentAttack -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void Shoot(Vector3 direction, Transform launcher)
+    {
+        Rigidbody2D projectileInstance;
+        projectileInstance = Instantiate(projectile, launcher.position, Quaternion.identity);
+        projectileInstance.AddForce(direction * this.BulletSpeed);
+        IsAttackReady = false;
+    }
+}
